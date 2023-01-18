853e8f0dd198cbe2b3c4731f35c22c57b6a6b2f0
zehra <zehradastan@hotmail.com>
2023-01-18 22:43:16 +0000

    second question added

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -7,5 +7,14 @@ const question = [
         {text:'alert',correct:true},
         {text:'numbers',correct:false}
       ]
+    },
+    {
+        question:'The condation in an if/else statement is enclose within',
+        answer:[
+            {text:'quotes',correct:false},
+            {text:'curly brackets',correct:false},
+            {text:'paranthesas',correct:true},
+            {text:'square brakets',correct:false}
+          ]
     }
   ]
