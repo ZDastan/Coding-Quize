0a867121874c6b935fbbc9b391594cd853df1435
zehra <zehradastan@hotmail.com>
2023-01-18 17:55:06 +0000

    start event added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,13 @@
+const startButton =document.getElementById('start-btn')
+startButton.addEventListener('click', startQuize)
 
-    
+function startQuize () {
+console.log('started')
+}
+function setNextQuestion() {
+
+}
+function selectAnswer() {
+
+}
+ 
