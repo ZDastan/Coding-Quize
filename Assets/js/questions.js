b0a3bde6652991b4ea3465c7507576d207d0aeeb
zehra <zehradastan@hotmail.com>
2023-01-18 22:35:38 +0000

    question move to question.js

---
+++ Assets/js/questions.js
@@ -0,0 +1,11 @@
+const question = [
+    {
+      question:'Common use data types DO NOT include:',
+      answer:[
+        {text:'strings',correct:false},
+        {text:'booleans',correct:false},
+        {text:'alert',correct:true},
+        {text:'numbers',correct:false}
+      ]
+    }
+  ]
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -61,16 +61,6 @@ function clearStatusClass(element){
 
 startButton.onclick = startQuiz
 
-const question = [
-  {
-    question:'Common use data types DO NOT include:',
-    answer:[
-      {text:'strings',correct:false},
-      {text:'booleans',correct:false},
-      {text:'alert',correct:true},
-      {text:'numbers',correct:false}
-    ]
-  }
-]
+
 
  
