d4efd182990cc8ca4a43b978d50815d6ed5cd100
zehra <zehradastan@hotmail.com>
2023-01-18 22:28:06 +0000

    js updateed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,76 @@
-const startButton =document.getElementById('start-btn')
-startButton.addEventListener('click', startQuize)
+const startButton =document.getElementById('start')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement =document.getElementById('answer-buttons')
 
-function startQuize () {
+var shuffledQuestions, currentQuestionIndex
+
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz () {
 console.log('started')
+startButton.classList.add('hide')
+questionElement.classList.remove('hide')
+shuffledQuestions=question.sort(()=> Math.random()- .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
+
 function setNextQuestion() {
+  
+  showQuestion(shuffledQuestion[currentQuestionIndex])
 
 }
+function showQuestion(question){
+  questionElement.innerText = question.question
+  question.answer.forEach(answer =>{
+    const button = document.createElement('button')
+    button.classList.add('btn')
+    if(answer.correct){
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+
+}
+
 function selectAnswer() {
-
+const selectButton = e.target
+const correct = selectButton.dataset.correct
+setStatusClass(document.body, correct)
+Array.from(answerButtonsElemnt.children).forEach(button => {
+  setStatusClass(button, button.dataset.correct)
+})
 }
+function clearStatusClass(element, correct){
+  clearStatusClass(element)
+  if(correct){
+    element.classList.add('correct')
+  }else{
+    element.classList.add('wrong')
+  }
+  
+}
+function clearStatusClass(element){
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+startButton.onclick = startQuiz
+
+const question = [
+  {
+    question:'Common use data types DO NOT include:',
+    answer:[
+      {text:'strings',correct:false},
+      {text:'booleans',correct:false},
+      {text:'alert',correct:true},
+      {text:'numbers',correct:false}
+    ]
+  }
+]
+
  
