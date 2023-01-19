84f5fe07be14161b8f9871eeefb2d1586e08c997
zehra <zehradastan@hotmail.com>
2023-01-19 20:06:56 +0000

    highscore updateed

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,1 +1,17 @@
+function populateHighScores() {
+    
+    var highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
+  
+    var list = '';
+    highScoresList.forEach(score => {
+        list = list + '<p>' + score.initials + '  :  ' + score.score + '</p>';
+    });
+    highScoresListEl.innerHTML = list;
+}
 
+function resetScores() {
+    localStorage.clear();
+    populateHighScores();
+}
+
+populateHighScores();
